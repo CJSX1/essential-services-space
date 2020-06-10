1a7aff27972c25cd641c17f3d0f21d9acbe393ad
Jaq <suraj@docintosh.com>
2020-06-10 11:07:21 +0530

    js changes php composer

---
+++ assets/js/traits.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+
+    let days_arr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    var d = new Date();
+    var n = d.getDay();
+    $("." + days_arr[n]).html("<span class='nav-live'>(Live)</span> " + days_arr[n]);
+    $("." + days_arr[n]).addClass("active active-locked");
+
+    $("h2", "#" + days_arr[n]).html(days_arr[n] + " <span class='title-live'>(Live)</span>");
+
+    fadeThem();
+})
+
+function fadeThem() {
+    $(".nav-live").fadeOut(500, function () {
+        $(this).fadeIn(200, fadeThem());
+    });
+
+    $(".title-live").fadeOut(500, function () {
+        $(this).fadeIn(200, fadeThem());
+    });
+}
