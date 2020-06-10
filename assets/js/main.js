2c3817518a2ae56e70bbcbfa43a1aed53d5f2725
Jaq <suraj@docintosh.com>
2020-06-10 12:36:27 +0530

    filter files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Hyperspace by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function ($) {
 
 	var $window = $(window),
@@ -50,7 +44,6 @@
 		var $sidebar_a = $sidebar.find('a');
 
 		$sidebar_a
-			.addClass('scrolly')
 			.on('click', function () {
 
 				var $this = $(this);
@@ -77,114 +70,7 @@
 				// No section for this link? Bail.
 				if ($section.length < 1)
 					return;
-
-				// Scrollex.
-				$section.scrollex({
-					mode: 'middle',
-					top: '-20vh',
-					bottom: '-20vh',
-					initialize: function () {
-
-						// Deactivate section.
-						$section.addClass('inactive');
-
-					},
-					enter: function () {
-
-						// Activate section.
-						$section.removeClass('inactive');
-
-						// No locked links? Deactivate all links and activate this section's one.
-						if ($sidebar_a.filter('.active-locked').length == 0) {
-
-							$sidebar_a.removeClass('active');
-							$this.addClass('active');
-
-						}
-
-						// Otherwise, if this section's link is the one that's locked, unlock it.
-						else if ($this.hasClass('active-locked'))
-							$this.removeClass('active-locked');
-
-					}
-				});
-
 			});
 
 	}
-
-	// Scrolly.
-	$('.scrolly').scrolly({
-		speed: 1000,
-		offset: function () {
-
-			// If <=large, >small, and sidebar is present, use its height as the offset.
-			if (breakpoints.active('<=large')
-				&& !breakpoints.active('<=small')
-				&& $sidebar.length > 0)
-				return $sidebar.height();
-
-			return 0;
-
-		}
-	});
-
-	// Spotlights.
-	$('.spotlights > section')
-		.scrollex({
-			mode: 'middle',
-			top: '-10vh',
-			bottom: '-10vh',
-			initialize: function () {
-
-				// Deactivate section.
-				$(this).addClass('inactive');
-
-			},
-			enter: function () {
-
-				// Activate section.
-				$(this).removeClass('inactive');
-
-			}
-		})
-		.each(function () {
-
-			var $this = $(this),
-				$image = $this.find('.image'),
-				$img = $image.find('img'),
-				x;
-
-			// Assign image.
-			$image.css('background-image', 'url(' + $img.attr('src') + ')');
-
-			// Set background position.
-			if (x = $img.data('position'))
-				$image.css('background-position', x);
-
-			// Hide <img>.
-			$img.hide();
-
-		});
-
-	// Features.
-	$('.features')
-		.scrollex({
-			mode: 'middle',
-			top: '-20vh',
-			bottom: '-20vh',
-			initialize: function () {
-
-				// Deactivate section.
-				$(this).addClass('inactive');
-
-			},
-			enter: function () {
-
-				// Activate section.
-				$(this).removeClass('inactive');
-
-			}
-		});
-
 })(jQuery);
